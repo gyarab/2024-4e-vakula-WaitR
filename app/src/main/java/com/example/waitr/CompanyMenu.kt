836ab86f8471459432238c9d8d7dc22246f990b9
Michael Vakula <michael.vakula@student.gyarab.cz>
2024-12-26 01:04:04 +0100

    adding feature to delete companies + authorization logic

--- app/src/main/java/com/example/waitr/CompanyMenu.kt
+++ app/src/main/java/com/example/waitr/CompanyMenu.kt
@@ -1,9 +1,11 @@
 package com.example.waitr
 
+import android.app.AlertDialog
 import android.app.Dialog
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.Gravity
 import android.view.MenuItem
 import android.view.View
 import android.widget.Button
@@ -14,11 +16,14 @@ import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isEmpty
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
 import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
+// trida pro ulozeni  tagu pro button
+data class CompanyTag(val companyId: String, val companyName: String, val authorization: String)
 
 class CompanyMenu : AppCompatActivity() {
     private lateinit var drawerLayout: DrawerLayout
@@ -31,6 +36,11 @@ class CompanyMenu : AppCompatActivity() {
     private val userId = currentUser?.uid
     private val db = FirebaseDatabase.getInstance("https://waitr-dee9a-default-rtdb.europe-west1.firebasedatabase.app/").reference // Using Realtime Database reference
     private lateinit var createCompanyPopup: Button
+    private var selectedCompanyId: String? = null
+    private var selectedCompanyName: String? = null
+    private var selectedAuthorization: String? = null
+    private var selectedCompanyTag: CompanyTag? = null
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -99,18 +109,20 @@ class CompanyMenu : AppCompatActivity() {
                         for (companySnapshot in dataSnapshot.children) {
                             val companyId = companySnapshot.key
                             val companyName = companySnapshot.child("companyName").getValue(String::class.java)
+                            val authorization = companySnapshot.child("Authorization").getValue(String::class.java)
 
-                            if (companyId != null && companyName != null) {
+                            if (companyId != null && companyName != null && authorization != null) {
                                 val newButton = Button(this).apply {
                                     text = companyName
-                                    tag = companyId // Uložení ID podniku
+                                    tag = CompanyTag(companyId, companyName, authorization)
                                 }
-
                                 newButton.setOnClickListener {
-                                    val companyIdasString = it.tag as String
-                                    val intent = Intent(this, Company_manager::class.java)
-                                    intent.putExtra("COMPANY_ID", companyIdasString)
-                                    startActivity(intent)
+                                    val companyTag = it.tag as CompanyTag
+                                    selectedCompanyTag = companyTag
+                                    selectedCompanyId = companyTag.companyId // Aktualizace ID
+                                    selectedCompanyName = companyTag.companyName
+                                    selectedAuthorization = companyTag.authorization
+                                    selectedCompanyOptions()
                                 }
 
                                 // Přidání tlačítka do LinearLayout
@@ -141,7 +153,6 @@ class CompanyMenu : AppCompatActivity() {
             (resources.displayMetrics.heightPixels * 0.6).toInt()
         )
 
-        // Zarovnání na střed
         dialog.window?.setBackgroundDrawableResource(android.R.color.holo_blue_light)
 
         // Reference na prvky v popup layoutu
@@ -153,32 +164,35 @@ class CompanyMenu : AppCompatActivity() {
         createButton.setOnClickListener {
             val companyName = companyNameInput.text.toString().trim()
             if (companyName.isNotEmpty()) {
+                selectedCompanyName = companyName
                 if (userId != null) {
                     // vytvoreni id pro spolecnost
                     val companyId = db.child("users").child(userId).child("companies").push().key
 
                     if (companyId != null) {
+                        selectedCompanyId = companyId // Nastavení ID do globální proměnné
                         // Vytvoř nový Button
                         val newButton = Button(this).apply {
+                            selectedCompanyName = companyName
                             text = companyName
-                            tag = companyId // Uložení ID podniku (například z databáze)
+                            //tag = companyId // Uložení ID podniku
+                            tag = CompanyTag(companyId, companyName, "manager")
                         }
-
                         newButton.setOnClickListener {
-                            val companyIdasString = it.tag as String // Získání ID podniku
-                            val intent = Intent(this, Company_manager::class.java)
-                            intent.putExtra(
-                                "COMPANY_ID",
-                                companyIdasString
-                            ) // Předání ID do nové aktivity
-                            startActivity(intent)
+                            val companyTag = it.tag as CompanyTag
+                            selectedCompanyTag = companyTag
+                            selectedCompanyId = companyTag.companyId // Aktualizace ID
+                            selectedCompanyName = companyTag.companyName
+                            selectedAuthorization = companyTag.authorization
+                            selectedCompanyOptions()
                         }
                         // Přidání tlačítka do LinearLayout
                         linearLayoutContainer.addView(newButton)
 
                         // Uložení názvu společnosti a ID do Firebase Realtime Database u konkrétního uživatele
                         val companyMap = mapOf(
-                            "companyName" to companyName
+                            "companyName" to companyName,
+                            "Authorization" to "manager"
                         )
 
                         // Uložení společnosti do uživatele
@@ -209,6 +223,84 @@ class CompanyMenu : AppCompatActivity() {
         // Zobrazení dialogu
         dialog.show()
     }
+    private fun selectedCompanyOptions() {
+        // Vytvoření dialogu
+        val dialog1 = Dialog(this)
+        dialog1.setContentView(R.layout.selected_company_options)
+
+        // Nastavení velikosti dialogu
+        dialog1.window?.setLayout(
+            (resources.displayMetrics.widthPixels * 0.65).toInt(),
+            (resources.displayMetrics.heightPixels * 0.4).toInt()
+        )
+
+        dialog1.window?.setBackgroundDrawableResource(android.R.color.white)
+
+        val enterButton = dialog1.findViewById<Button>(R.id.enter_company_button)
+        val deleteButton = dialog1.findViewById<Button>(R.id.delete_company_button)
+        val selectedCompanyNameView = dialog1.findViewById<TextView>(R.id.selected_company_name)
+
+        selectedCompanyNameView.apply {
+            text = selectedCompanyName
+        }
+
+        enterButton.setOnClickListener{
+            val intent: Intent
+            if (selectedAuthorization == "manager"){
+                intent = Intent(this, Company_manager::class.java)
+                intent.putExtra("COMPANY_ID", selectedCompanyId) // Předání ID do nové aktivity
+                startActivity(intent)
+            }
+        }
+
+        deleteButton.setOnClickListener{
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Delete Company")
+            builder.setMessage("Are you sure you want to delete this company?")
+
+            builder.setPositiveButton("Yes") { dialog, _ ->
+                deleteCompany(selectedCompanyId!!, selectedCompanyTag!!)
+                dialog.dismiss()
+                dialog1.dismiss()
+            }
+
+            builder.setNegativeButton("No") { dialog, _ ->
+                dialog.dismiss()
+            }
+
+            val alertDialog = builder.create()
+            alertDialog.show()
+        }
+        dialog1.show()
+    }
+
+    private fun deleteCompany(companyId: String, CompanyTag: CompanyTag){
+        val linearLayoutContainer = findViewById<LinearLayout>(R.id.linearLayoutContainer)
+        if (userId != null) {
+            db.child("users").child(userId).child("companies").child(companyId).removeValue()
+                .addOnSuccessListener {
+                    val buttonToRemove = linearLayoutContainer.findViewWithTag<Button>(CompanyTag)
+                    linearLayoutContainer.removeView(buttonToRemove)
+                    Toast.makeText(this, "Company deleted successfully!", Toast.LENGTH_SHORT).show()
+                }
+                .addOnFailureListener {
+                    Toast.makeText(this, "Failed to delete company: ${it.message}", Toast.LENGTH_SHORT).show()
+                }
+        }
+    }
+/* zatim nevyuzito
+    private fun checksIfNoCompanies(){
+        val linearLayoutContainer = findViewById<LinearLayout>(R.id.linearLayoutContainer)
+        if (linearLayoutContainer.isEmpty()) {
+            val textView = TextView(this).apply {
+                text = "No companies yet"
+                textSize = 16f
+                gravity = Gravity.CENTER
+            }
+            linearLayoutContainer.addView(textView)
+        }
+    }
+*/
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return if (drawerToggle.onOptionsItemSelected(item)) {
             true // Událost byla zpracována výsuvným menu
