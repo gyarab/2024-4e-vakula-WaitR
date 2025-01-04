13d50178fb720d72ef21d48fb28edad3a5caa734
Michael Vakula <michael.vakula@student.gyarab.cz>
2025-01-04 19:01:02 +0100

    created layout for editing the menu

--- app/src/main/java/com/example/waitr/Food_menu.kt
+++ app/src/main/java/com/example/waitr/Food_menu.kt
@@ -1,13 +1,16 @@
 package com.example.waitr
 
+import android.app.Dialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.TextView
 
 class Food_menu : Fragment() {
-    //promenne
+    // globalni promenne
     private var CompanyID: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,20 +24,19 @@ class Food_menu : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view = inflater.inflate(R.layout.fragment_food_menu, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_food_menu, container, false)
+        return view
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val editButton = view.findViewById<ImageButton>(R.id.edit_menu_button)
+        editButton.setOnClickListener {
+            showEditMenuPopup()
+        }
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Food_menu.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(companyId: String) =
             Food_menu().apply {
@@ -43,4 +45,34 @@ class Food_menu : Fragment() {
                 }
             }
     }
+
+    private fun showEditMenuPopup(){
+// Vytvoření dialogu
+        val dialog = Dialog(requireContext())
+        dialog.setContentView(R.layout.edit_food_menu_popup)
+
+        // Nastavení velikosti dialogu
+        dialog.window?.setLayout(
+            (resources.displayMetrics.widthPixels * 0.95).toInt(),
+            (resources.displayMetrics.heightPixels * 0.95).toInt()
+        )
+        // Reference na prvky v popup layoutu
+        val saveButton = dialog.findViewById<TextView>(R.id.save_menu_edit)
+        val cancelButton = dialog.findViewById<TextView>(R.id.cancel_menu_edit)
+        val addItemButton = dialog.findViewById<TextView>(R.id.add_menu_item)
+        val addGroupButton = dialog.findViewById<TextView>(R.id.add_menu_group)
+        saveButton.setOnClickListener {
+
+        }
+        cancelButton.setOnClickListener {
+
+        }
+        addItemButton.setOnClickListener {
+
+        }
+        addGroupButton.setOnClickListener {
+
+        }
+    }
+
 }
