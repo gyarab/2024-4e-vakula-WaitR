5b2eb77dd4757ffbf4d485d460d35e172ab85a04
Michael Vakula <michael.vakula@student.gyarab.cz>
2025-03-16 17:29:45 +0100

    background service

--- app/src/main/java/com/example/waitr/BackgroundService.kt
+++ app/src/main/java/com/example/waitr/BackgroundService.kt
@@ -3,9 +3,18 @@ package com.example.waitr
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import android.util.Log
+import android.widget.Button
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 //TODO
 class BackgroundService : Service() {
+
+    private val auth = FirebaseAuth.getInstance()
+    private val currentUser = auth.currentUser
+    private val userId = currentUser?.uid
+    private lateinit var companyId: String
+    private val db = FirebaseDatabase.getInstance("https://waitr-dee9a-default-rtdb.europe-west1.firebasedatabase.app/").reference // Using Realtime Database reference
 
     override fun onBind(intent: Intent?): IBinder? {
         return null
@@ -14,20 +23,42 @@ class BackgroundService : Service() {
     override fun onTaskRemoved(rootIntent: Intent?) {
         super.onTaskRemoved(rootIntent)
         // Aplikace byla ukončena
-        updateUserStatus(false) // Nastav stav uživatele na "offline"
+        updateUserStatus() // Nastav stav uživatele na "offline"
         stopSelf()
     }
 
-    private fun updateUserStatus(isOnline: Boolean) {
-        val userId = getCurrentUserId() // Získej ID aktuálního uživatele
-        if (userId != null) {
-            val userRef = FirebaseDatabase.getInstance().getReference("users").child(userId).child("online")
-            userRef.setValue(isOnline)
+    private fun updateUserStatus() {
+        getCompanyId { id ->
+            companyId = id
+            val ref = userId?.let { db.child("companies").child(companyId).child("users").child(it).child("status") }
+            ref?.setValue("offline")?.addOnSuccessListener {
+                Log.e("success", "uspech")
+            }
         }
     }
 
-    private fun getCurrentUserId(): String? {
-        // Získej ID aktuálního uživatele (např. z SharedPreferences nebo Firebase Auth)
-        return "currentUserId" // Nahraď skutečným ID uživatele
+    private fun getCompanyId(callback: (String) -> Unit) {
+        val companyList = mutableListOf<String>()
+        val ref = userId?.let { db.child("users").child(it).child("companies") }
+        ref?.get()?.addOnSuccessListener { dataSnapshot ->
+            if (dataSnapshot.exists()) {
+                for (child in dataSnapshot.children) {
+                    companyList.add(child.key.toString())
+                }
+                val idRef = db.child("companies")
+                companyList.forEach { company ->
+                    val reference = userId?.let { idRef.child(company).child("users").child(it) }
+                    reference?.get()?.addOnSuccessListener { dataSnapshot ->
+                        if (dataSnapshot.exists()) {
+                            val status = dataSnapshot.child("status").getValue(String::class.java)
+                            if (status == "online") {
+                                callback(company)
+                                return@addOnSuccessListener
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
